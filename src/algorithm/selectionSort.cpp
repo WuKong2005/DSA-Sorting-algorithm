360e4b56bc9a6810014ad514d1726206b8975d02
TCMeen01 <tcminh23@clc.fitus.edu.vn>
2024-06-28 17:36:48 +0700

    check algorithms

--- src/algorithm/selectionSort.cpp
+++ src/algorithm/selectionSort.cpp
@@ -1,7 +1,7 @@
 #include "selectionSort.h"
 
 void selectionSort(int a[], int n) {
-    for (int i = 0; i < n - 2; i++) { 
+    for (int i = 0; i < n - 1; i++) { 
         int min = i; 
         for (int j = i + 1; j < n; j++) { 
             if (a[j] < a[min]) 
@@ -12,7 +12,7 @@ void selectionSort(int a[], int n) {
 }
 
 void selectionSort(int a[], int n, long long &comparison) {
-    for (int i = 0; ++comparison && (i < n - 2); i++) { 
+    for (int i = 0; ++comparison && (i < n - 1); i++) { 
         int min = i; 
         for (int j = i + 1; ++comparison && (j < n); j++) { 
             if (++comparison && (a[j] < a[min])) 
